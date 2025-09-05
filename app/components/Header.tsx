0f6f646b976d903dc22d49100fed91b454b0f9a0
romain noyer <romain.noyer@gmail.com>
2025-09-05 17:54:13 +0200

    update header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -9,15 +9,24 @@ export default function Header() {
     return (
         <header className="sticky top-0">
             <div className="navbar bg-base-100 shadow-sm">
-                <div className="navbar-start">
+                <div className="navbar-start flex-1">
                 </div>
-                <div className="navbar-center">
+                <div className="navbar-center flex-1">
                     <Tabs />
                 </div>
-                <div className="navbar-end">
-                    <Link href="/starter" className="btn">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2.5" stroke="currentColor" className="size-[1.2em]"><path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" /></svg>
+                <div className="navbar-end flex-1">
+                    <Link href="/starter" className="btn group overflow-hidden">
                         Contact
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth={1.5}
+                            stroke="currentColor"
+                            className="size-6 -rotate-45 group-hover:animate-fly">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5" />
+                        </svg>
+
                     </Link>
                 </div>
             </div>
